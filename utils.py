aeefbb6e5c13cee4bbcf7330e9faf36027f4d158
harizaldycahya <zaldypratama82@gmail.com>
2022-12-18 16:33:19 +0700

    fix add pages

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ from streamlit.components.v1 import html
 from PATHS import NAVBAR_PATHS, SETTINGS
 
 
-# def inject_custom_css():
-#     with open('assets/styles.css') as f:
-#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+def inject_custom_css():
+    with open('assets/styles.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
 def get_current_route():
@@ -18,8 +18,8 @@ def get_current_route():
 
 
 def navbar_component():
-    # with open("assets/images/settings.png", "rb") as image_file:
-    #     image_as_base64 = base64.b64encode(image_file.read())
+    with open("assets/images/settings.png", "rb") as image_file:
+        image_as_base64 = base64.b64encode(image_file.read())
 
     navbar_items = ''
     for key, value in NAVBAR_PATHS.items():

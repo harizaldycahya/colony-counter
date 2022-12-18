5e3a3085a69ea9dbb5f7fc1fd72a4a1edefa0205
harizaldycahya <zaldypratama82@gmail.com>
2022-12-18 18:00:28 +0700

    adding pages

---
+++ pages/About.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("# Plotting Demo")
+st.sidebar.header("About")
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)

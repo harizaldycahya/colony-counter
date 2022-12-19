5cc615ca384cc9c3153a300862854c8e87246e24
harizaldycahya <zaldypratama82@gmail.com>
2022-12-19 18:57:20 +0700

    test

--- Home.py
+++ Home.py
@@ -26,29 +26,26 @@ link = '[Start Counting Colony](https://harizaldycahya-colony-counter-home-ng37y
 st.markdown(link, unsafe_allow_html=True)
 
 
-t = st.radio("Toggle to see font change", [True, False])
-
-if t:
-    st.markdown(
-        """
+st.markdown(
+    """
         <style>
-@font-face {
-  font-family: 'Rubik Gemstones';
-  font-style: normal;
-  font-weight: 400;
-  src: url(https://fonts.googleapis.com/css2?family=Rubik+Gemstones&display=swap);
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
+        @font-face {
+        font-family: 'Rubik Gemstones';
+        font-style: normal;
+        font-weight: 400;
+        src: url(https://fonts.googleapis.com/css2?family=Rubik+Gemstones&display=swap);
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
 
-    html, body, [class*="css"]  {
-    font-family: 'Rubik Gemstones';
-    font-size: 48px;
+        html, body, [class*="css"]  {
+        font-family: 'Rubik Gemstones';
+        font-size: 48px;
     }
     </style>
 
     """,
         unsafe_allow_html=True,
-    )
+)
 
 "# Hello"
 

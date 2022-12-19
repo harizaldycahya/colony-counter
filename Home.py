a31328a1db799f342d222304356266c6f852bfa3
harizaldycahya <zaldypratama82@gmail.com>
2022-12-19 18:22:34 +0700

    test

--- Home.py
+++ Home.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 hidemenu =  """
 <style>
+
+    @import url('https://fonts.googleapis.com/css2?family=Rubik+Gemstones&display=swap');
     #MainMenu{
         visibility:hidden;
     }
@@ -14,17 +16,9 @@ hidemenu =  """
         position:relative;
         color:tomato;
     }
-    @font-face {
-        font-family: 'Tangerine';
-        font-style: normal;
-        font-weight: 400;
-        src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZM.woff2) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-    }
 
     html, body {
-        font-family: 'Tangerine';
-        font-size: 48px;
+        font-family: 'Rubik Gemstones', cursive;
     }
 
 

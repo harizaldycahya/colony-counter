e0e4fdad73f9d27863bea677ee21b77c730531b7
harizaldycahya <zaldypratama82@gmail.com>
2022-12-19 19:05:38 +0700

    test

--- Home.py
+++ Home.py
@@ -29,19 +29,16 @@ st.markdown(link, unsafe_allow_html=True)
 st.markdown(
     """
         <style>
-        @font-face {
-        font-family: 'Rubik Gemstones';
-        font-style: normal;
-        font-weight: 400;
-        src: url(https://fonts.googleapis.com/css2?family=Rubik+Gemstones&display=swap);
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-        }
+            @font-face {
+            font-family: 'Poppins';
+            src: url(https://fonts.googleapis.com/css2?family=Poppins&display=swap);
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            }
 
-        html, body, [class*="css"]  {
-        font-family: 'Rubik Gemstones';
-        font-size: 48px;
-    }
-    </style>
+            html, body, [class*="css"]  {
+                font-family: 'Poppins';
+            }
+        </style>
 
     """,
         unsafe_allow_html=True,

1c2e43573cf1b96743d00c0275a7703d06b07c16
harizaldycahya <zaldypratama82@gmail.com>
2023-01-21 11:26:22 +0700

    no class feedback

--- detect.py
+++ detect.py
@@ -159,6 +159,9 @@ def run(
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
+                    # Tambahan edok
+                     if(names[0] == ''):
+                        st.text('Tidak ada kelas terdeteksi')
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format

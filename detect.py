0537c5c668b9f41d7549cb6efaec010226f602e4
Harizaldy <47654458+harizaldycahya@users.noreply.github.com>
2023-02-06 05:48:52 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -55,8 +55,8 @@ def run(
         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
+        conf_thres=0.313,  # confidence threshold
+        iou_thres=0.5,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results

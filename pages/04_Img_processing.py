fc3728fd424d693aeea00d8ac6d63f777b6444a6
harizaldycahya <zaldypratama82@gmail.com>
2023-03-14 14:09:53 +0700

    test

--- pages/04_Img_processing.py
+++ pages/04_Img_processing.py
@@ -28,29 +28,9 @@ footer:after{
 </style>
 """
 
-
-def get_subdirs(b='.'):
-    '''
-        Returns all sub-directories in a specific Path
-    '''
-    result = []
-    for d in os.listdir(b):
-        bd = os.path.join(b, d)
-        if os.path.isdir(bd):
-            result.append(bd)
-    return result
-
-
-def get_detection_folder():
-    '''
-        Returns the latest folder in a runs\detect
-    '''
-    return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
-
-
 if __name__ == '__main__':
 
-    st.markdown(hidemenu,unsafe_allow_html=True)
+    # st.markdown(hidemenu,unsafe_allow_html=True)
     st.title('Automatic Bacteria Colony Forming Units Counter')
     st.write('Note : **:blue[The image resolution should be above 800px to get the best results]**')
 
